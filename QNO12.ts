2c5fb1dc0b116ddfd3f75346dd34ab4c11490303
Aqsagull99 <159878357+Aqsagull99@users.noreply.github.com>
2024-02-17 00:42:03 +0500

    Add files via upload

---
+++ QNO12.ts
@@ -0,0 +1,11 @@
+//  QNO:12
+// Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+
+// let student:string[]=["naila","kashaf","qirat","noreen"]
+
+// let massage:string="How are you?"
+
+// console.log(student[0] + " " + massage)
+// console.log(student[1] + " " + massage)
+// console.log(student[2] + " " + massage)
+// console.log(student[3] + " " + massage)
